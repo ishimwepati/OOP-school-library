0017599f17999d76c5e46f1f2d84defbe54a07de
ishimwepati <wazacode@gmail.com>
2023-10-19 20:44:42 +0300

    STudent updated

--- class/student.rb
+++ class/student.rb
@@ -1,14 +1,21 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name: 'Unknown', parent_permission: true)
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @classroom = classroom
+    @classroom = nil
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
   end
+
+  def join_classroom(classroom)
+    @classroom = classroom
+    classroom.add_student(self)
+  end
 end
+
